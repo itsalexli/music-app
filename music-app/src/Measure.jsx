0c53dba4c012051104c7e3843f1d20edf9df9e6c
kevinli5371 <kevinli5371@gmail.com>
2025-01-03 23:58:36 -0500

    made bars dyanmic when 6 or more notes are in the measure

--- music-app/src/Measure.jsx
+++ music-app/src/Measure.jsx
@@ -1,12 +1,35 @@
 import MeasurePic from "./assets/measure.png";
+import BarPic from "./assets/bar.png";
 import Note from "./Note";
+import { measureNum, noteList } from "./App";
+const notePerBar = 6; //pre determined number of notes per bar image
+
+
+function createNoteCountDictionary() { //need to account for a bar with 0 notes
+  let noteDictionary = {}; //creates a dictionary where the key is the measure # and the value is the number of notes in that measure
+  for (let i = 0; i < noteList.length; i++){
+    if (!(noteList[i].measure in noteDictionary)){
+      noteDictionary[noteList[i].measure] = 1;
+    }
+    else{
+      noteDictionary[noteList[i].measure] += 1;
+    }
+  }
+  return noteDictionary;
+}
 
 function Measure({ measure, notes }) {
   // Filter notes relevant to this measure
   const measureNotes = notes.filter((note) => note.measure === measure);
+  const measureBarCount = Math.floor(createNoteCountDictionary()[measure]/notePerBar);
+  console.log(measureBarCount);
 
   return (
     <div className="measure-container">
+      <img className="bar" src={BarPic} alt={`Bar ${measure}`} />
+      {Array.from({ length: measureBarCount }).map((_, index) => (
+        <img className="measure" src={MeasurePic} alt={`Measure ${measure}`} />
+      ))}
       <img className="measure" src={MeasurePic} alt={`Measure ${measure}`} />
       <ul className="note-container">
         {measureNotes.map((note, index) => (
@@ -15,6 +38,7 @@ function Measure({ measure, notes }) {
           </li>
         ))}
       </ul>
+      <img className="bar" src={BarPic} alt={`Bar ${measure}`} />
     </div>
   );
 }
